2e432f1c188cd78bf1cb2eabf90458ab9b294179
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-12 18:21:10 -0600

    table user, blog, and comment has been created and migrated.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+Blog.destroy_all
+Comment.destroy_all
+
+# Users
+jane = User.create(username: "janeur", password: "Testing12345#")
+jimmy = User.create(username: "jvng", password: "Testing12345$")
+
+# Blogs
+post_one = Blog.create(title: "First Post", content: "Hello online world. It is nice to code!", user: jane)
+post_two = Blog.create(title: "Second Post", content: "This is a big headache. I am so stressed out.", user: jane)
+post_three = Blog.create(title: "Third Post", content: "Work is so demanding! Can't wait to get home to my family.", user: jimmy)
+
+# Comments
+one = Comment.create(content: "You got this!", user: jimmy, blog: post_two)
+two = Comment.create(content: "Miss you babe", user: jane, blog: post_three)
+three = Comment.create(content: "Miss you too", user: jimmy, blog: post_three)
+four = Comment.create(content: "Hello there!", user: jimmy, blog: post_one)
+five = Comment.create(content: "Hello there to you Jimmy!", user: jane, blog: post_one)
+six = Comment.create(content: "How is your day?", user: jimmy, blog: post_one)
+seven = Comment.create(content: "It's going pretty well. Been coding all day and I am pooped!", user: jane, blog: post_one)
